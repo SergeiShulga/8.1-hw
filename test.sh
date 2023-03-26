61a7acb3f2477c6694f04a75c54f53e02bb69d80
SergeiShulga <mygley73@gmail.com>
2023-03-26 23:43:02 +0400

    commit -a

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # git commit -m 'First commit'
-
+1
 # git commit -m 'First commit1'
-
+2
 # git commit -m 'First commit2'
-
+3
 # git commit -m 'First commit3'
-
+4
 # git commit -m 'First commit4'
